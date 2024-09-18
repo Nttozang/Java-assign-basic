dc18cf18fc185d8b89f1fa94330cfe1a8a6f296e
nut <suppanat.nut@gmail.com>
2024-09-18 19:59:43 +0700

    first commit

---
+++ src/numberCheck.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class numberCheck {
+    public static void main(String[] args) {
+        Scanner myNumber = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+
+        int check = myNumber.nextInt();
+        if (check % 2 == 0){
+            System.out.println("Number is EVEN");
+        }else {
+            System.out.println("Number is Odd");
+        }
+    }
+}
