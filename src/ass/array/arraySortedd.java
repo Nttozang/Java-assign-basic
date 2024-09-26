6b4fb7285c7f5eb79c9699d571331a8a7e2e4d3e
nut <suppanat.nut@gmail.com>
2024-09-26 21:32:49 +0700

    add-ex1-array

---
+++ src/ass/array/arraySortedd.java
@@ -0,0 +1,19 @@
+package ass.array;
+
+import java.util.Arrays;
+
+public class arraySortedd {
+    public static void main(String[] args){
+        int[] myNum = {100,200,50,1000,15,648};
+        Arrays.sort(myNum);
+        for (int i : myNum){
+            System.out.println("num :"+i);
+        }
+
+        String[] cars = {"Volvo", "BMW", "Tesla", "Ford", "Fiat", "Mazda", "Audi"};
+        Arrays.sort(cars);
+        for (String c : cars){
+            System.out.println("car :"+c);
+        }
+    }
+}
